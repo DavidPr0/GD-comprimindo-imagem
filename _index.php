c5f7bb0797b3b9f5fe49c0873786102ac24cc83f
DavidPr0 <davidpaco2.4@gmail.com>
2019-05-15 23:09:28 -0300

    v.1.0.0

---
+++ _index.php
@@ -0,0 +1,37 @@
+<?php 
+
+$arquivo = "IMG_4278.JPG";
+
+$width = 200;
+$height = 200;
+
+
+list($largura_original, $altura_original) = getimagesize($arquivo);
+
+$ratio = $largura_original / $altura_original;
+
+if ($width/$height > $ratio) {
+	$width = $height * $ratio;
+}else{
+	$height = $width / $ratio;
+}
+
+// echo "L ORG: ".$largura_original." - A ORIG: ".$altura_original."<br>";
+
+// echo "Largura: ".$width." - Altura: ".$heigt;
+
+$imagem_final = imagecreatetruecolor($width, $height);
+
+$imagem_original = imagecreatefromjpeg($arquivo);
+
+imagecopyresampled($imagem_final, $imagem_original, 0, 0, 0, 0, $width, $height, $largura_original, $altura_original);
+
+// Transformar o arquivo php em arquivo imagem
+// header("Content-Type: image/jpeg");
+
+// imagejpeg($imagem_final,null,100);
+
+// Salva imagem
+imagejpeg($imagem_final,"mini_imagem.jpeg");
+
+echo "Imagem redimencionada com sucesso";
